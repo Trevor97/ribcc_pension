010944f6afb7527d84df17226c8553121c7bd521
Trevor97 <trevorsikaloka15@gmail.com>
2022-07-28 14:18:07 +0200

    removed autocomplete js

--- beneficiaries.php
+++ beneficiaries.php
@@ -5,6 +5,7 @@
 <html lang="en"> 
 <head>
     <title>Beneficiaries | Pension GLA System</title>	
+	<script src="lib/autocomplete_member.js"></script>
 	<?php
 		/** head includes; css, meta etc */
 		include_once("lib/head_includes.php")
@@ -163,47 +164,6 @@
 	<script>
 		/** set nav item active */
 		document.getElementById("overview_nav").classList.add('active');
-
-		function get_text(event){
-			var string = event.textContent;
-			document.getElementsByName('member_name')[0].value = string;
-
-			document.getElementById('search_result').innerHTML = '';
-		}
-
-		function load_data(query){
-			if(query.length >0){
-				var form_data = new FormData();
-
-				form_data.append('query', query);
-
-				var ajax_request = new XMLHttpRequest();
-
-				ajax_request.open('POST', 'lib/autocomplete_member.php');
-
-				ajax_request.send(form_data);
-
-				ajax_request.onreadystatechange = function(){
-					if(ajax_request.readyState == 4 && ajax_request.status == 200){
-						var response = JSON.parse(ajax_request.responseText);
-
-						var html = '<div class="list-group">';
-							if(response.length > 0){
-								for(var count = 0; count < response.length; count++){
-									html += '<a href="#" class="list-group-item list-group-item-action" onclick="get_text(this)">'+response[count].member_name+'</a>';
-								}
-							}else{
-								html += '<a href="#" class="list-group-item list-group-item-action disabled">No Results Found</a>';
-							}
-						html += '</div>';
-
-						document.getElementById('search_result').innerHTML = html;
-					}
-				}
-			}else{
-				document.getElementById('search_result').innerHTML = '';
-			}
-		}
 	</script>
 </body>
 </html> 
