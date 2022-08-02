94d51b5c05537c57320b266990d8d68d30cf16e2
Trevor97 <trevorsikaloka15@gmail.com>
2022-08-02 14:37:00 +0200

    added Claims page

---
+++ claims.php
@@ -0,0 +1,51 @@
+<?php
+	include_once("lib/dashboard_functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>Members | Pension GLA System</title>
+	<?php
+		/** head includes; css, meta etc */
+		include_once("lib/head_includes.php")
+	?>
+</head> 
+
+<body class="app">   	
+    
+    <?php
+		/** side bar, search bar & notifications */
+		include_once("lib/nav_content.php")
+	?>
+
+    <div class="app-wrapper">
+	       
+    </div><!--//app-wrapper-->    					
+
+ 
+    <!-- Javascript -->          
+    <script src="assets/plugins/popper.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
+
+    <!-- Charts JS -->
+    <script src="assets/plugins/chart.js/chart.min.js"></script> 
+    <script src="assets/js/index-charts.js"></script> 
+    
+    <!-- Page Specific JS -->
+    <script src="assets/js/app.js"></script> 
+
+	<!-- Various page scripts -->
+	<script>
+		/** set nav item active */
+		document.getElementById("overview_nav").classList.add('active');
+
+		/** auto update stat-cards */
+		// var auto_refresh = setInterval(
+		// 	function (){
+		// 		$('#client_count').load('').fadeIn("slow");
+		// 	}, 1000);
+
+	</script>
+</body>
+</html> 
+
